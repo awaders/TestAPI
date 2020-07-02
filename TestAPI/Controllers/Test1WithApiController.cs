49f8d31e921844bc84f38babee217414d134fe6b
awaders <67753112+awaders@users.noreply.github.com>
2020-07-02 12:24:11 -0400

    Add files via upload

---
+++ TestAPI/Controllers/Test1WithApiController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
+
+namespace TestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Test1WithApiController : ControllerBase
+    {
+        // GET: api/Test1WithApi
+        [HttpGet]
+
+        [EnableCors("PolicyAllowGet")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Test1", "WithApi" };
+        }
+
+        
+    }
+}
---
+++ TestAPI/Controllers/Test2WithXxxController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
+
+namespace TestAPI.Controllers
+{
+    [Route("xxx/[controller]")]
+    [ApiController]
+    public class Test2WithXxxController : ControllerBase
+    {
+        // GET: ap/Test2WithXxx
+        [HttpGet]
+        [EnableCors("PolicyAllowGet")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Test2", "WithXxx" };
+        }
+
+    }
+}
